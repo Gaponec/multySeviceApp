222dc192f59c1b46f7815cfc0b1bffc689af829e
Bohdan Liakhovetskyi <bohdan.liakhovetskyi-external@sema4genomics.com>
2019-11-13 12:22:56 +0200

    Initial commit

---
+++ city-service/src/main/java/com/gaponec/CityServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gaponec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CityServiceApplication.class, args);
+  }
+}
